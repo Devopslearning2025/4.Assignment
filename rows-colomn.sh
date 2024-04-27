61f20ae38449fd767ff3e0590c97fab708718f7a
devops <dev0pslearning2025@gmail.com>
2024-04-27 10:45:39 +0530

    assignment

--- rows-colomn.sh
+++ rows-colomn.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-table=" name age 
-ramm 32
-rahim 33
-alla 34"
+table="
+Name Age
+Ramesh 32
+Suresh 33
+Naresh 34"
 
-transpose_table=$(echo "$table"|awk '{
+my_table=$(echo "$table"|awk '{
     for (i = 1; i <= NF; i++) {
         if (NR == 1){
             row[i] = $i
@@ -20,29 +21,6 @@ transpose_table=$(echo "$table"|awk '{
     }')
 
 ## Print the transposed matrix
-echo "$transpose_table"
+echo "$my_table"
 
-#!/bin/bash
-
-# Original matrix
-#matrix="1 2 3
-#4 5 6
-#7 8 9"
-
-## Transpose the matrix
-#transpose_matrix=$(echo "$matrix" | awk '{
- #   for (i = 1; i <= NF; i++) {
-  #      if (NR == 1) {
-   #         row[i] = $i
-    #    } else {
-     #       row[i] = row[i] " " $i
-      #  }
-    #}
-#} END {
- #   for (i = 1; i <= NF; i++) {
-  #      print row[i]
-   # }
-#}')
-
-## Print the transposed matrix
-#echo "$transpose_matrix"
+#cat table |awk '{for (i = 1; i <= NF; i++) { if (NF == 1) { row[i] = $i} } else { row[i] = row[i] " " $i } {print $i}}'
