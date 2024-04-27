5b1b4fef846215c8f88e0015bf156ed2db39e2e4
devops <dev0pslearning2025@gmail.com>
2024-04-27 12:45:34 +0530

    assignment

--- word-count.sh
+++ word-count.sh
@@ -8,14 +8,9 @@
 
 # Assign the test file to a variable
 #test_file=$1
-test_file=[ "EC2" "Dashboard" "EC2" "Global" "View" "Events" "Console-to-Code"
-"Preview Instances "Instances" "Instances" "Types" "Launch" "Templates" "Spot" "Requests"
-"Savings" "Plans Reserved" "Instances" "Dedicated" "Hosts"
-"Capacity" "Reservations" "New" "Images"
-"Instances"
-"Reservations"
-"Savings"
-"Preview" "Instances" "Global" "Events" "Instances" "Dashboard" ]
+test_file=[ "EC2" "Dashboard" "EC2" "Global" "View" "Events" "Console-to-Code" "Preview" "Instances" "Instances" "Instances" "Types" "Launch" "Templates" "Spot" "Requests"
+"Savings" "Plans Reserved" "Instances" "Dedicated" "Hosts"m"Capacity" "Reservations" "New" "Images"
+"Instances" "Reservations" "Savings" "Preview" "Instances" "Global" "Events" "Instances" "Dashboard" ]
 
 # Use awk to split the words, count occurrences, and print the result
 awk '{for (i=1; i<=NF; i++) words[$i]++} END {for (word in words) print word, words[word]}' "$test_file" \
