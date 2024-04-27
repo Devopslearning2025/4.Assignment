7180b7270cd739694c513ef20b10296367b28a4b
devops <dev0pslearning2025@gmail.com>
2024-04-27 11:59:27 +0530

    assignment

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-SOURCE=/tmp/ram/
-DESTINATION=/tmp/
+SOURCE=/tmp/ram/app-logs
+DESTINATION=/tmp/ram/
 DATE=$(date  +%F-%H-%M-%S)
 
 #Zipping the source directory in the destination folder
 tar -cvzf ${SOURCE}-${DATE}.tar.gz $SOURCE
 
 #Moving to destination directory
-cd /tmp
-mv ${SOURCE}-${DATE}.tar.gz $DESTINATION
+cd /tmp/ram
+mv *.tar.gz $DESTINATION
