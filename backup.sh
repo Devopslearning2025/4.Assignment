ab84e170d0d0721082904ff952d6ea33d61e351f
devops <dev0pslearning2025@gmail.com>
2024-04-27 11:40:57 +0530

    assignment

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ DESTINATION=/tmp/
 DATE=$(date +%F-%H-M%-S%)
 
 #Zipping the source directory in the destination folder
-tar -xvzf ${SOURCE}-${DATE}.tar.gz $SOURCE
+tar -xvzf ${SOURCE}-${DATE}.tar.gz 
 
 #Moving to destination directory
 mv ${SOURCE}-${DATE}.tar.gz $DESTINATION
