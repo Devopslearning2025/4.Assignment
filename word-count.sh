33fe667a00713fabeafa818f84dab7e09f9c207a
devops <dev0pslearning2025@gmail.com>
2024-04-27 12:49:40 +0530

    assignment

--- word-count.sh
+++ word-count.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 # Check if the words file is giving as a argument file
-#if [ "$#" -ne 1 ]; then
- #   echo "Please provide the file which has words"
-  #  exit 1
-#fi
+if [ "$#" -ne 1 ]; then
+   echo "Please provide the file which has words"
+   exit 1
+fi
 
 # Assign the test file to a variable
-#test_file=$1
-word=( EC2 Dashboard EC2 Global View Events Console-to-Code Preview Instances Instances Instances
-Types Launch Templates Spot Requests
-Savings Plans Reserved Instances Dedicated HostsmCapacity Reservations New Images
-Instances Reservations Savings Preview Instances Global Events Instances Dashboard )
+test_file=$1
 
 # Use awk to split the words, count occurrences, and print the result
-awk '{for (i=1; i<=NF; i++) words[$i]++} END {for (word in words) print word, words[word]}' "$word" \
+awk '{for (i=1; i<=NF; i++) words[$i]++} END {for (word in words) print word, words[word]}' "$test_file" \
 | sort -k2,2nr \
 | head -n 5
