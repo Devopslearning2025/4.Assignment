97ed1cf1eb9179ccd330b9757a433e62dbc1f995
devops <dev0pslearning2025@gmail.com>
2024-04-27 12:18:53 +0530

    assignment

--- backup.sh
+++ backup.sh
@@ -8,7 +8,8 @@ DATE=$(date  +%F-%H-%M)
 read -p "Enter source directory: " source
 
 #Zipping the source directory and moving into destination folder
-tar -cvzf ${DESTINATION}${source}-${DATE}.tar.gz $source
+#cd $SOURCE
+tar -cvzf ${DESTINATION}/${DATE}_backup.tar.gz $source
 
 #Moving to destination directory
 #cd /tmp/ram
