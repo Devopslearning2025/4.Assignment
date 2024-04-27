6e0771f3b2b710a146890382daa119e522031a19
devops <dev0pslearning2025@gmail.com>
2024-04-27 11:28:08 +0530

    assignment

--- cpu.sh
+++ cpu.sh
@@ -2,7 +2,7 @@
 
 #to get the top 5 cpus
 #cpu=$(ps -eo pid.ppid,cmd,%mem,%cpu --sort=%cpu|head -6)
-cpu=$(ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%cpu|head -5)
+cpu=`ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%cpu|head -5`
 
 echo "cpu"
 
